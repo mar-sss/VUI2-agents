5e8150c3ddb4273c6428ea9d6b35287286ebf77f
Martin Suss <martinsuss95@gmail.com>
2018-12-29 18:14:49 +0100

    2: Auto stash before rebase of "Kolomaznik/master"

--- src/main/java/cz/mendelu/vui2/agents/ReactionAgent.java
+++ src/main/java/cz/mendelu/vui2/agents/ReactionAgent.java
@@ -7,17 +7,56 @@ import java.util.regex.Pattern;
 
 public class ReactionAgent extends AbstractAgent {
 
-    boolean firstAction = false;
+    private boolean firstAction = false;
 
-    StringBuilder actionList = new StringBuilder();
+    private StringBuilder actionList = new StringBuilder();
+    private int initBattery = 1000;
+    private int battery = initBattery;
+    private boolean goBack = false;
 
     @Override
     public Action doAction(boolean canMove, boolean dirty, boolean dock) {
+        if(canMove){
+            canMove = false;
+        } else{
+            canMove = true;
+        }
+        battery--;
+        if(!goBack && battery <= ((initBattery/2)+2)){ //+2 because of rotation!
+            goBack = true;
+            reverseActions();
+            if(getLastAction() == 'F'){ //if yes, make a rotation = 2 rotations
+                actionList.append('L');
+                actionList.append('L');
+            }
+
+            // if last action was rotation, then continue with the same rotation again to be in the correcct direction
+            // why replace = because we have reversed actions!
+            if(getLastAction() == 'R'){
+                actionList.replace(actionList.length()-1, actionList.length(),"L");
+            } else if(getLastAction() == 'L'){
+                actionList.replace(actionList.length()-1, actionList.length(),"R");
+            }
+        }
+
+        if (goBack){
+            char action = actionList.toString().charAt(0);
+            actionList.deleteCharAt(actionList.length()-1);
+            switch (action){
+                case 'F': return Action.FORWARD;
+                case 'R': return Action.TURN_RIGHT;
+                case 'L': return Action.TURN_LEFT;
+                default: return Action.TURN_OFF;
+            }
+
+        }
+
+
 
         // if it is the first action of the agent, let leave the dock
         if(!firstAction){
             if(dirty){ // if there is dirt in the dock, clean it at first
-                actionList.append("C");
+                //actionList.append("C");
                 return Action.CLEAN;
             }
             if(canMove) { // if I can move from dock, go forward
@@ -25,27 +64,30 @@ public class ReactionAgent extends AbstractAgent {
                 actionList.append("F");
                 return Action.FORWARD;
             } else{ //if I can't move at the beginning, let rotate right
-                firstAction = true; // this was my first move
                 actionList.append("R");
                 return Action.TURN_RIGHT;
             }
         }
 
         // exit the task, I am in the dock again
-        if (dock){
-                actionList.append("D");
-                return Action.DONE;
-        }
+        //if (firstAction && dock){
+        //    actionList.append("D");
+        //    return Action.TURN_OFF;
+        //}
 
         // clean the dirt if there is some
         if(dirty){
-            actionList.append("C");
+            //actionList.append("C");
             return Action.CLEAN;
         }
 
         if(canMove){ // (canMove && !dirty)
             if(actionList.substring(actionList.length()-1,actionList.length()).equals("F")){ // previous action = forward?
                 if(actionList.length()>1 && actionList.substring(actionList.length()-2,actionList.length()-1).equals("R")){ // last rotation = right?
+                    if(actionList.substring(actionList.length()-3,actionList.length()-2).equals("R")){
+                        //TODO
+                    }
+
                     actionList.append("L");
                     return Action.TURN_LEFT;
                 } else{
@@ -76,6 +118,22 @@ public class ReactionAgent extends AbstractAgent {
         return 'u';
 
     }
+
+
+    private char getLastAction(){
+        return actionList.substring(actionList.length()-1,actionList.length()).toCharArray()[0];
+    }
+
+    private void reverseActions(){
+        String s = actionList.toString().replaceAll("R","l").replaceAll("L","r").replaceAll("l","L").replaceAll("r","R");
+        actionList = new StringBuilder(s);
+
+        //dont make cleaning again!
+        s = actionList.toString().replaceAll("C","");
+        actionList = new StringBuilder(s);
+
+    }
+
     public static void main(String [ ] args)
     {
         ReactionAgent reactionAgent =  new ReactionAgent();
--- src/main/java/cz/mendelu/vui2/agents/WorldAgent.java
+++ src/main/java/cz/mendelu/vui2/agents/WorldAgent.java
@@ -2,10 +2,24 @@ package cz.mendelu.vui2.agents;
 
 import cz.mendelu.vui2.agents.greenfoot.AbstractAgent;
 
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
 public class WorldAgent extends AbstractAgent {
+
+    static char WALL = 'X';
+    static char DIRT = '*';
+    static char DOCK = '_';
+    //static EnumSet<Character> GROUND;
+
+    ArrayList<ArrayList<Character>> world;
 
     @Override
     public Action doAction(boolean canMove, boolean dirty, boolean dock) {
+
+        world = new ArrayList<>();
+
         return null;
     }
 }
